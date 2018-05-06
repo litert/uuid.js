aa786b84124d2218b61cbff1dce2b0c2690fbaec
Angus.Fenying <i.am.x.fenying@gmail.com>
2018-05-06 18:02:24 +0800

    Added copyrights comments.

--- src/algorithms/snowflake-si-va.ts
+++ src/algorithms/snowflake-si-va.ts
@@ -1,3 +1,18 @@
+/*
+   +----------------------------------------------------------------------+
+   | LiteRT UUID.js Library                                               |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2018 Fenying Studio                                    |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.0 of the Apache license,    |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | https://github.com/litert/uuid.js/blob/master/LICENSE                |
+   +----------------------------------------------------------------------+
+   | Authors: Angus Fenying <fenying@litert.org>                          |
+   +----------------------------------------------------------------------+
+ */
+
 export namespace SnowflakeSIvA {
 
     export interface UUIDGenerateor {
